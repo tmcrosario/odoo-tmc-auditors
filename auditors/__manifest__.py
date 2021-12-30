dd078d571a4998e599ad3fc03a36e939c1d16521
Lisandro Gallo <4266083+lisandrogallo@users.noreply.github.com>
2021-12-30 09:47:16 -0300

    [MIG] Migration to 14.0

--- auditors/__manifest__.py
+++ auditors/__manifest__.py
@@ -1,22 +1,18 @@
 {
-    'name': "TMC Auditors",
-    'summary': 'Registro de Auditores Externos',
-    'version': '13.0.1.0.0',
-    'website': 'https://www.tmcrosario.gob.ar',
-    'author': 'Tribunal Municipal de Cuentas - Municipalidad de Rosario',
-    'license': 'AGPL-3',
-    'depends': [
-        'tmc',
-        'partner_tmc',
-        'partner_fiscal'
+    "name": "TMC Auditors",
+    "summary": "Registro de Auditores Externos",
+    "version": "14.0.1.0.0",
+    "website": "https://www.tmcrosario.gob.ar",
+    "author": "Tribunal Municipal de Cuentas - Municipalidad de Rosario",
+    "license": "AGPL-3",
+    "depends": ["tmc", "partner_tmc", "partner_fiscal"],
+    "data": [
+        "security/auditors_groups.xml",
+        "security/ir.model.access.csv",
+        "views/partner_views.xml",
+        "views/auditors_menus.xml",
+        "views/institution_views.xml",
+        "views/institution_menus.xml",
     ],
-    'data': [
-        'security/groups.xml',
-        'security/ir.model.access.csv',
-        'views/partner_views.xml',
-        'views/auditors_menus.xml',
-        'views/institution_views.xml',
-        'views/institution_menus.xml',
-    ],
-    'demo': [],
+    "demo": [],
 }  # yapf: disable
